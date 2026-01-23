95af6553d0a25e89d2c74690061c7175c85828fa
eundoo <leeeunj00@naver.com>
2026-01-23 16:03:38 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const swiper = new Swiper(".main-center .swiper", {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+const swiper1 = new Swiper(".kuki-original-2 .swiper", {
+  loop: true,
+  slidesPerView: 4.5,
+  spaceBetween: 20,
+  speed: 9000,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+  },
+  freeMode: true,
+  freeModeMomentum: false,
+  allowTouchMove: true,
+});
